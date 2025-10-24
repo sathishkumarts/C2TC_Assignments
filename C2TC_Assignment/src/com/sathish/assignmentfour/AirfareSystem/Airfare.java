48ea54597ac14ea42461c78ee6dac18503fa54cc
Sathishkumarts <sathishkumarts0109@gmail.com>
2025-10-24 12:30:21 +0530

    assignment 4 completed

---
+++ C2TC_Assignment/src/com/sathish/assignmentfour/AirfareSystem/Airfare.java
@@ -0,0 +1,6 @@
+package com.sathish.assignmentfour.AirfareSystem;
+
+public interface Airfare {
+    Double calculateAmount(); // abstract method
+    void display();            // method to display details
+}
